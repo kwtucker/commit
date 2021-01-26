1d8974ccad8722b8e40f262f506be2d972923534
kevin_tucker2 <kevin_tucker2@comcast.com>
2021-01-26 09:53:12 -0500

    update

--- commit.go
+++ commit.go
@@ -1,6 +1,9 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 func GetCommits(modifiedFiles []string) []string {
 	out := []string{}
@@ -12,9 +15,21 @@ func GetCommits(modifiedFiles []string) []string {
 
 	// Parse Files
 	for _, filename := range modifiedFiles {
-		UnStageFile(filename)
-		out = append(out, ReadFile(filename)...)
-		StageFile(filename)
+		fi, err := os.Stat(filename)
+		if err != nil {
+			fmt.Println(err)
+			return out
+		}
+		switch mode := fi.Mode(); {
+		case mode.IsDir():
+			// TODO: cd into the directory and read files
+			// do directory stuff
+			fmt.Println("directory")
+		case mode.IsRegular():
+			UnStageFile(filename)
+			out = append(out, ReadFile(filename)...)
+			StageFile(filename)
+		}
 	}
 
 	return out
